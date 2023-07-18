4f8450558dbf90dca9f474718ba0a038e17dd6ff
Jayden <sqwatato@gmail.com>
2023-07-18 14:31:05 -0700

    class name

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,48 +6,16 @@ from django.contrib.auth.forms import UserCreationForm
 from phonenumber_field.formfields import PhoneNumberField
 
 class ClientSignUpForm(UserCreationForm):
-    img = forms.ImageField(
-            widget=forms.FileInput(attrs={'class': 'form-control'})
-    )
+    img = forms.ImageField()
+    username = forms.CharField(max_length=30, required=True,)
+    first_name = forms.CharField(max_length=100,)
+    last_name = forms.CharField(max_length=100,)
+    email = forms.EmailField(max_length=150,)    
+    phone_number = PhoneNumberField()
+    address = forms.CharField(max_length=100, )
+    password1 = forms.CharField()
     
-    username = forms.CharField(
-        max_length=30, 
-        required=True,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    
-    first_name = forms.CharField(
-        max_length=100,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    
-    last_name = forms.CharField(
-        max_length=100,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    
-    email = forms.EmailField(
-        max_length=150,
-        widget=forms.EmailInput(attrs={'class': 'form-control'})
-    )
-        
-    
-    phone_number = PhoneNumberField(
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    
-    address = forms.CharField(
-        max_length=100, 
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    
-    password1 = forms.CharField(
-        widget=forms.PasswordInput(attrs={'class': 'form-control'})
-    )
-    
-    password2 = forms.CharField(
-        widget=forms.PasswordInput(attrs={'class': 'form-control'})
-    )
+    password2 = forms.CharField()
 
     class Meta:
         model = User
@@ -72,6 +40,40 @@ class ClientSignUpForm(UserCreationForm):
             user=user,
         )
         return requester
+    def __init__ (self, *args, **kwargs):
+        super(ClientSignUpForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget = forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Username'
+        })
+        self.fields['first_name'].widget = forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'First Name'
+        })
+        self.fields['last_name'].widget = forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Last Name'
+        })
+        self.fields['email'].widget = forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Email'
+        })
+        self.fields['address'].widget = forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Address'
+        })
+        self.fields['phone_number'].widget = forms.TextInput(attrs={
+            'class': 'form-control',
+        })
+        self.fields['img'].widget = forms.FileInput(attrs={
+            'class': 'form-control',
+        })
+        self.fields['password1'].widget = forms.PasswordInput(attrs={
+            'class': 'form-control',
+        })
+        self.fields['password2'].widget = forms.PasswordInput(attrs={
+            'class': 'form-control',
+        })
     
 class BusinessSignUpForm(forms.ModelForm):
     name = forms.CharField(max_length=150)
